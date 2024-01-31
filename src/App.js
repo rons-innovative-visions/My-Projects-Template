f43f6bdc80ca9acb74e92bd9a449a488e2ec9745
Ron <ronsinnovativevisions@gmail.com>
2024-01-31 15:35:37 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {projects} from './projects'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id="title">
+        <h1>My Projects</h1>
+        <a href="#projects"><span></span></a>
+      </section>
+      <section id="projects">
+        {projects.map((project) => {
+          return <Project {...project} key={project.id}/>
+        })}
+      </section>
+    </>
   );
+}
+
+const Project = ({title, img, vid, url, id}) => {
+  const showVideo = () => {
+    document.getElementById(title).querySelector('video').classList.add('show');
+  }
+  const hideVideo = () => {
+    document.getElementById(title).querySelector('video').classList.remove('show');
+  }
+  
+  return (
+    <article id={title} className='project' key={id}>
+      <h2>{title}</h2>
+      <a href={url} target='_blank'>
+        <img src={img} alt="..." onMouseEnter={showVideo}/>
+        <video autoPlay muted loop onMouseLeave={hideVideo}>
+          <source src={vid} type='video/mp4'/>
+        </video>
+      </a>
+    </article>)
 }
 
 export default App;
---
+++ src/projects.js
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: 'Project 1',
+        img: 'https://dummyimage.com/hd720',
+        vid: './project1.mp4',
+        url: 'http://localhost:3000/',
+        id: 1
+    },
+    {
+        title: 'Project 2',
+        img: 'https://dummyimage.com/hd720',
+        vid: './project1.mp4',
+        url: 'http://localhost:3000/',
+        id: 2
+    },
+]
